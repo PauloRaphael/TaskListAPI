8e5ea928289bd7b97d39544e711127ed5a5f838b
Paulo Brito <paulo.raphael.web@gmail.com>
2025-10-05 10:04:30 -0300

    Initial Commit

---
+++ TaskListAPI/Model/Entities/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskListAPI.Model.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
---
+++ TaskListAPI/Model/Entities/LoginSessao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaskListAPI.Model.Entities
+{
+    public class LoginSessao
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string Token { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime ExpiraEm { get; set; }
+    }
+}
